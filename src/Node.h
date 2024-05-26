28f0db44d653de2b6be750201487438b576df329
Dominick1998 <54410726+Dominick1998@users.noreply.github.com>
2024-05-25 22:58:34 -0500

    Update Node.h
    comments

--- src/Node.h
+++ src/Node.h
@@ -1,20 +1,28 @@
 #ifndef NODE_H
 #define NODE_H
 
-#include <iostream>
-using namespace std;
+#include <iostream> // Include the input-output stream library for any potential I/O operations
+using namespace std; // Use the standard namespace
 
+// Node class definition
 class Node {
+    // Declare DoublyLinkedList as a friend class to allow it access to Node's private members
     friend class DoublyLinkedList;
+
 public:
+    // Default constructor
     Node();
+
+    // Parameterized constructor to initialize the node with a value
     Node(int v);
+
+    // Destructor
     ~Node();
 
 private:
-    Node* prev;
-    int value;
-    Node* next;
+    Node* prev;   // Pointer to the previous node in the doubly linked list
+    int value;    // Value/data stored in the node
+    Node* next;   // Pointer to the next node in the doubly linked list
 };
 
-#endif
+#endif // NODE_H
